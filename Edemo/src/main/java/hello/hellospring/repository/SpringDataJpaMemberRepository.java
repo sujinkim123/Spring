8bc0d1b4b9ff4b74b472f48fc8fb38f5628767a3
alice <alice@DESKTOP-RI9ICPL>
2021-09-16 16:29:54 +0900

    스프링입문1

---
+++ Edemo/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import hello.hellospring.domain.Member;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+	
+	// JPQL select m from Member m where m.name = ?
+	@Override
+	Optional<Member> findByName(String name);
+
+}
